a24701807acb097c4a0aa60653f16dc23bb25441
KapOraal <djibril.dieng@isen-lille.fr>
2016-11-20 21:23:52 +0100

    Correction for Windows

--- src/main.py
+++ src/main.py
@@ -37,19 +37,23 @@ def addFolder(folderState, ftpClient):
 
 def handleFolder(folder, foldersBeforeRefresh, ftpClient):
     """Handles a folder in the synchronization process"""
-    if (folder in foldersBeforeRefresh):
-        rmFolder(folder, ftpClient)
+
+    folderWithDoubleBackSlash = folder.replace("\\","\\\\")
+    logging.info(folderWithDoubleBackSlash)
+
+    if (folderWithDoubleBackSlash in foldersBeforeRefresh):
+        rmFolder(folderWithDoubleBackSlash, ftpClient)
         if (os.path.exists(folder)):
             if (debugMode):
-                logging.info("Folder modified: " + folder)
-            addFolder(os.walk(folder), ftpClient)
+                logging.info("Folder modified: " + folderWithDoubleBackSlash)
+            addFolder(os.walk(folderWithDoubleBackSlash), ftpClient)
         else:
             if (debugMode):
-                logging.info("Folder deleted: " + folder)
+                logging.info("Folder deleted: " + folderWithDoubleBackSlash)
     else:
         if (debugMode):
-            logging.info("Folder added: " + folder)
-        addFolder(os.walk(folder), ftpClient)
+            logging.info("Folder added: " + folderWithDoubleBackSlash)
+        addFolder(os.walk(folderWithDoubleBackSlash), ftpClient)
 
 def handleFile(filepath, filesBeforeRefresh, ftpClient):
     """Handles a file in the synchronization process"""
@@ -92,13 +96,7 @@ if (__name__ == "__main__"):
     maxDepth = namespace.__getattribute__(constants.MAX_DEPTH_OPTION)
     debugMode = namespace.__getattribute__(constants.DEBUG_OPTION)
 
-    # creating and configuring
-    ftpClient = ftplib.FTP(ftpServerUrl)
-    ftpClient.login(user=username, passwd=password)
-    ftpClient.set_debuglevel(1)
-
-    # adding the folder to the server
-    addFolder(os.walk(localFolder), ftpClient)
+    sys.stdout = open(logFile, 'w')
 
     # configuring the logger
     logging.basicConfig(filename=logFile,
@@ -107,18 +105,56 @@ if (__name__ == "__main__"):
                         datefmt='%H:%M:%S',
                         level=logging.DEBUG)
 
+    # creating and configuring
+    ftpClient = ftplib.FTP(ftpServerUrl)
+    ftpClient.login(user=username, passwd=password)
+    ftpClient.set_debuglevel(1)
+
+    # adding the folder to the server
+    #On verifie si le fichier existe deja avant de le creer
+    path = "C:/Users/isen/Desktop/repertoireFTP/localfolder/"
+    if (os.path.exists(path)):
+        logging.info("Le fichier local est deja present sur le site distant")
+    else :
+        logging.info("Creation du dossier local sur le site distant")
+        addFolder(os.walk(localFolder), ftpClient)
+
+    if not (debugMode):
+        logging.info("debug_inactive")
+
+
     while (True):
-        foldersBeforeRefresh = [x[0] for x in os.walk(localFolder)]
-        filesBeforeRefresh = sum([x[2] for x in os.walk(localFolder)], [])
+
+        foldersBeforeRefresh = [x[0] for x in os.walk('localFolder')]
+
+        logging.info("Folder Before Refresh")
+
+        logging.info(foldersBeforeRefresh)
+
+        filesBeforeRefresh = sum([x[2] for x in os.walk('localFolder')], [])
+
+        logging.info("FILES Before Refresh")
+
+        logging.info(filesBeforeRefresh)
+
         handledFolders = []
+
         timestampBeforeRefresh = time.time()
+
         time.sleep(refreshFrequency)
+
         folderState = os.walk(localFolder)
+
+
         for root, dirs, files in folderState:
-            if (isNew(root, timestampBeforeRefresh) and not isAlreadyHandled(root, handledFolders)):
+            if (not isAlreadyHandled(root, handledFolders)):
+                
                 handleFolder(root, foldersBeforeRefresh, ftpClient)
+
                 handledFolders.append(root)
-            for filename in files:
-                filepath = os.path.join(root, filename)
-                if (isNew(filepath, timestampBeforeRefresh)):
+
+                for filename in files:
+
+                    filepath = os.path.join(root, filename)
+
                     handleFile(filepath, filesBeforeRefresh, ftpClient)
